5da90b4596331df6be7bc40e7915ef337a13591b
EC2 Default User <ec2-user@ip-172-31-21-203.ap-northeast-2.compute.internal>
2016-03-08 02:25:10 +0000

    analyzing done

--- hello.py
+++ hello.py
@@ -1,26 +1,56 @@
 from flask import Flask, render_template, request
-from modules import *
+from pymongo import MongoClient
+
+#Korean Analyzer
+from konlpy.tag import Twitter
+import json
+from bson import json_util
 
 app = Flask(__name__)
 
 @app.route("/")
 def enter():
-    result = render_template('enter.html')
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.reviews
+    collection = db.app
+    data = list()
+    for appid in collection.distinct('appid'):
+        appInfo = dict()
+        appInfo['appid'] = appid
+        appInfo['apptitle'] = collection.find_one({'appid':appid})['apptitle']
+        data.append(appInfo)
+    result = render_template('enter.html',data = data)
     return result
 
 ## html - main.html
 @app.route("/main")
 def main():
     id = request.args.get('id')
-    limit = request.args.get('limit')
-    result = render_template('main.html',data=getReview(int(limit), id))
+    result = render_template('main.html',data=loadReview(id),id=id)
     return result
 
-@app.route("/test")
-def test():
-    limit = request.args.get('limit')
+## test - tokenizer
+@app.route("/test1")
+def test1():
     id = request.args.get('id')
-    return json.dumps(getReview(int(limit), id))
+    return json.dumps(list(loadReview(id)), default=json_util.default)
+
+## test - tokenizer
+@app.route("/test2")
+def test2():
+    text = request.args.get('text')
+    twitter = Twitter()
+    return json.dumps(twitter.pos(text, norm=True, stem=True))
+
+## function - Load Review from MongoDB
+def loadReview(id):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.reviews
+    collection = db.app
+    if id == 'all':
+        return collection.find({}).sort('no', 1).sort('appid',1)
+    else:
+        return collection.find({"appid":id}).sort('no', 1)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
--- modules.py
+++ modules.py
@@ -1,20 +1,43 @@
 #-*- coding: utf-8 -*-
 import requests
 import json
+import time
+import re
+import mechanize
 from BeautifulSoup import BeautifulSoup
+from pymongo import MongoClient
+from konlpy.tag import Twitter
+
+## Delete Reviews from MongoDB
+def deleteReview(id):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.reviews
+    collection = db.app
+    collection.remove({'appid':id})
+    print 'Deleted: ' + id
 
 ## Get Google Reviews By Page
-def getReview(limit, id):
+def saveReview(limit, id):
+    #appInfo
+    try:
+        appInfoResult = getAppInfo(id)
+    except:
+        print 'appInfoError - ' + id
+    #appReview
+    print 'begin: ' + id
     url = 'https://play.google.com/store/getreviews'
     headers = {
         'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
         'user-agent': 'Mozilla/5.0'
     }
 
-    results = list()
     pageNum = 0
     reviewNo = 1
     whileFlag = True
+
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.reviews
+    collection = db.app
 
     while whileFlag:
         data = {'reviewType':'0','pageNum':pageNum,'id':id,'reviewSortOrder':'0','xhr':'1'}
@@ -31,21 +54,124 @@ def getReview(limit, id):
             title = review.find('span',attrs={'class':'review-title'}).text
             body = title + ' ' + review.find('div',attrs={'class':'review-body'}).text[len(title):-5]
             rating = int(review.find('div',attrs={'class':'current-rating'})['style'].split(':')[1].strip()[:-2])/20
+            user = review.find('span',attrs={'class':'author-name'}).find('a')
+            user = user['href'].split('id=')[1] if user is not None else ''
+            result['apptitle'] = appInfoResult['title']
+            result['appgenre'] = appInfoResult['genre']
+            result['appid'] = id
             result['no'] = reviewNo
             result['author'] = author
             result['date'] = date
             result['rating'] = rating
             result['body'] = body
-            result['warning1'] = 'none'
-            result['warning2'] = 'none'
-            result['warning3'] = 'none'
-            result['warning4'] = 'none'
-            results.append(result)
+            result['user'] = user
+            collection.insert(result)
             if reviewNo >= limit:
                 whileFlag = False
                 break
             else:
                 reviewNo+=1
+        print 'page: ' + str(pageNum)
         pageNum+=1
+        time.sleep(10)
 
+    print 'saved: ' + str(collection.find({"appid":id}).count())
+
+def getAppInfo(id):
+    url = 'https://play.google.com/store/apps/details?id='+id
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'user-agent': 'Mozilla/5.0'
+    }
+    r = requests.post(url, headers=headers)
+    soup = BeautifulSoup(r.text)
+
+    appInfoResult = dict()
+    appInfoResult['title'] = soup.find('div',attrs={'class':'id-app-title'}).text
+    appInfoResult['genre'] = soup.find('a',attrs={'class':'document-subtitle category'}).text
+    return appInfoResult
+
+## function - Load Review from MongoDB
+def setReviewData(id):
+    print 'analyze begin - '+id
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.reviews
+    collection = db.app
+    if id == 'all':
+        data = collection.find({})
+    else:
+        data = collection.find({"appid":id})
+
+    for row in data:
+        collection.remove({'_id':row['_id']})
+        row['wordset'] = getTokenizedWords(row['body'])
+        row['shortness'] = checkWithRating(checkShortness(row['wordset']),row['rating'])
+        row['exaggeration'] = checkWithRating(checkExaggeration(row['wordset']),row['rating'])
+        row['reward'] = checkWithRating(checkReward(row['body']),row['rating'])
+        collection.insert(row)
+
+## function - tokenize
+def getTokenizedWords(text):
+    twitter = Twitter()
+    tokens = twitter.pos(text, norm=True, stem=True)
+    results = list()
+    for token in tokens:
+        if token[1] in ['Noun','KoreanParticle','Adjective']:
+            results.append(token[0])
+    results = list(set(results))
     return results
+
+## FraudCheck1 - Shortness
+def checkShortness(tokens):
+    lengthLimit = 5
+    result = len(tokens) < lengthLimit
+    return result
+
+## FraudCheck2 - Exaggeration
+def checkExaggeration(tokens):
+    mots = [u'최고',u'좋다',u'짱',u'별루']
+    intersectLimit = 0
+    result = len(mots) - len(list(set(mots) - set(tokens))) > intersectLimit
+    return result
+
+## FraudCheck3 - Reward
+def checkReward(body):
+    # 1 - mots check
+    result = False
+    mots = [u'쿠폰',u'이벤트',u'아이템',u'이모티콘',u'아이디',u'계정']
+    for mot in mots:
+        if mot in body:
+            result = True
+            break
+    # 2 - has email?
+    result = result or bool(re.search(r"(\w+[\w\.]*)@(\w+[\w\.]*)\.([A-Za-z]+)", body))
+    # 3 - has website? has id?
+    result = result or bool(re.match(r'([a-zA-Z]|[0-9]|[$-_@.&+])+', body))
+    return result
+
+## FraudCheck - With Rating
+def checkWithRating(result,rating):
+    if rating in [1,5] and result:
+        return 'High'
+    elif rating in [2,4] and result:
+        return 'Middle'
+    elif rating in [3] and result:
+        return 'Low'
+    else:
+        return ''
+
+ids = [
+        'com.sampleapp',
+        'com.chbreeze.jikbang4a',
+        'com.ftt.lostkaos',
+        'com.skylinematrix.ggplay.mhsykr',
+        'com.wanmei.dfws',
+        'com.lkkr.ltjdgg',
+        'com.teslacoilsw.launcher.prime',
+        'com.tribe.hondoom',
+        'com.mojang.minecraftpe'
+    ]
+for id in ids:
+    #deleteReview(id)
+    #saveReview(200,id)
+    setReviewData(id)
